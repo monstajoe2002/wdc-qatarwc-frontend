36e4e5526d2b462efda0436fb68ca492412f8e44
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-12-29 18:18:08 +0200

    ruotes are good to go 👍

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Reservations from "./components/Reservations";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="">
-     Hello
-    </div>
+//defin
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="reservations" element={<Reservations />} />  {/* */}
+      {/* define your own routes here */}
+    </Route>
   )
+);
+
+//router provider to wrap the app
+function App() {
+  return (
+    <>
+      {/* Navbar here */}
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter } from 'react-router-dom';
 import App from './App'
 import "./styles/index.css";
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
